aca22d10af494a902bc1f3b46dcf22d2ec426cf0
singlewhai <n.ratanasuwan@gmail.com>
2016-04-06 17:50:09 -0500

    add FtpConnect class

--- index.php
+++ index.php
@@ -2,9 +2,43 @@
     require_once('config.php');
     require_once('lib/FtpConnect.php');
 
-    echo FTP_DIRECTORY;
     $ftpConnect = new FtpConnect(FTP_HOSTNAME, FTP_PORT, FTP_TIMEOUT, FTP_USERNAME, FTP_PASSWORD, FTP_DIRECTORY);
-    echo $ftpConnect->connect();
-    var_dump($ftpConnect->getFileList());
+    echo $ftpConnect->connect().'<br>';
+
+    $date = new DateTime();
+    $dir = FTP_DEPLOY_DIR.'/'.$date->getTimestamp();
+    echo $dir.'<br>';
+
+    // create deploy directory
+    $ftpConnect->createDirectory(FTP_DEPLOY_DIR);
+
+    // dump
+    //var_dump($ftpConnect->getFileList());
+    echo '<br>';
+
+    // change current directory to deploy directory
+    $ftpConnect->setDirectory(FTP_DEPLOY_DIR);
+
+    // create directory
+    echo $ftpConnect->createDirectory($dir);
+    echo '<br>';
+    
+    // dump
+    //var_dump($ftpConnect->getFileList());
+    echo '<br>';
+
+    // delete empty directory
+    echo $ftpConnect->delete($dir);
+    echo '<br>';
+
+    // dump
+    //var_dump($ftpConnect->getFileList());
+    echo '<br>';
+
+    //var_dump($ftpConnect->uploadDirectory("/home/whaikung/code/project/public/git2directadmin",$dir));
+
     $ftpConnect->disconnect();
+
+    
 ?>
+
--- lib/FtpConnect.php
+++ lib/FtpConnect.php
@@ -10,23 +10,27 @@
         private $password;
         private $directory;
         private $ftp_conn;
+        private $blackList = array('.', '..', 'Thumbs.db', '.git', '.gitignore', 'README.md');
 
-        function JsonData($message, $statusCode){
+        public function JsonData($message, $statusCode){
             return json_encode(array('message' => $message, 'statusCode' => $statusCode));
         }
 
-        function __construct($hostname, $port, $timeout, $username, $password, $directory) {
+        public function __construct($hostname, $port, $timeout, $username, $password, $directory) {
             $this->hostname = $hostname;
             $this->port     = $port;
             $this->timeout  = $timeout;
             $this->username = $username;
             $this->password = $password;
             $this->directory= $directory;
-            $this->ftp_conn = null;
         }
-        
-        function connect(){
-            $this->ftp_conn = ftp_connect($this->hostname, $this->port, $this->timeout) or die("Could not connect to $ftp_server");
+
+        public function __destruct() {
+            $this->disconnect();
+        }
+
+        public function connect(){
+            $this->ftp_conn = ftp_connect($this->hostname, $this->port, $this->timeout) or die("Could not connect to $this->hostname");
             if (@ftp_login($this->ftp_conn, $this->username, $this->password))
             {
                 return $this->JsonData("Connection established.", 200);
@@ -37,26 +41,78 @@
             }
         }
 
-        function disconnect(){
-            ftp_close($this->ftp_conn);
+        public function disconnect(){
+            if (isset($this->ftp_conn)) {
+                ftp_close($this->ftp_conn);
+                unset($this->ftp_conn);
+            }        
+        }
+
+        public function setDirectory($path){
+            $this->directory = $path;
         }
 
         // get file list of current directory
-        function getFileList(){
+        public function getFileList(){
             return ftp_nlist($this->ftp_conn, $this->directory);     
         }
 
-        // try to delete file
-        function deleteFile($filename){
-            if (ftp_delete($this->ftp_conn, $filename))
+        // create derectory
+        public function createDirectory($directory){
+            if (ftp_mkdir($this->ftp_conn, $directory))
             {
-                return JsonData("$file deleted", 200);
+                return $this->JsonData("Successfully created $directory", 200);
             }
             else
             {
-                return JsonData("Could not delete $file", 404);
+                return $this->JsonData("Error while creating $directory", 404);
+            }
+        }
+        
+        // delete derectory
+        public function delete($path){
+            if(@ftp_rmdir($this->ftp_conn, $path) | @ftp_delete($this->ftp_conn, $path)){
+                return $this->JsonData("Directory $path was deleted", 200);
+            }else{
+                $files = ftp_nlist($this->ftp_conn, $path);
+                foreach ($files as $file)
+                {
+                    $this->delete($path.'/'.$file);
+                }
+                return $this->delete($path);
             }
         }
 
+        public function uploadDirectory($localPath, $remotePath){
+            return $this->recurse_directory($localPath, $localPath, $remotePath);
+        }
+
+        private function recurse_directory($rootPath, $localPath, $remotePath) {
+            $errorList = array();
+            if (!is_dir($localPath)) throw new Exception("Invalid directory: $localPath");
+            chdir($localPath);
+            $directory = opendir(".");
+            while ($file = readdir($directory)) {
+                if (in_array($file, $this->blackList)) continue;
+                if (is_dir($file)) {
+                    $errorList["$remotePath/$file"] = $this->createDirectory("$remotePath/$file");
+                    $errorList[] = $this->recurse_directory($rootPath, "$localPath/$file", "$remotePath/$file");
+                    chdir($localPath);
+                } else {
+                    $errorList["$remotePath/$file"] = $this->uploadFile("$localPath/$file", "$remotePath/$file");
+                }
+            }
+            return $errorList;
+        }
+
+        public function uploadFile($localPath, $remotePath) {
+            $error = "";
+            try {
+                ftp_put($this->ftp_conn, $remotePath, $localPath, FTP_BINARY); 
+            } catch (Exception $e) {
+                if ($e->getCode() == 2) $error = $e->getMessage(); 
+            }
+            return $error;
+        }
     }
 ?>
